ae5a78af64daf473021365364847260d6852db08
Marlon <marlon@marlonrondinelli@.com>
2024-09-21 10:42:22 -0700

    Add c/cpp formatter

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -2,16 +2,19 @@ return {
 	'stevearc/conform.nvim',
 	opts = {
 		formatters_by_ft = {
-			javascript = {'prettierd'},
-			javascriptreact = {'prettierd'},
-			typescript = {'prettierd'},
-			typescriptreact = {'prettierd'},
+			javascript = { 'prettierd' },
+			javascriptreact = { 'prettierd' },
+			typescript = { 'prettierd' },
+			typescriptreact = { 'prettierd' },
+			python = { 'black' },
+			c = { 'clang-format' },
+			cpp = { 'clang-format' }
 		},
 		format_on_save = {
 			lsp_format = "fallback",
 		}
 	},
 	keys = {
-		{'<F3>', function () require('conform').format({async=true}) end}
+		{ '<F3>', function() require('conform').format({ async = true }) end }
 	}
 }
